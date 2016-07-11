0328709e7fca60ac1d4094c03f25e66ca3893814
Joe William <jswilliam@gmail.com>
2016-07-11 16:14:31 +0200

    Added view for profile

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'page_controller#home'
 
+  resources :users
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
