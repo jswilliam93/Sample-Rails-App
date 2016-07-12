ac43b85b6122d2fbc36c18419d4f7d25c4639ea4
Joe William <jswilliam@gmail.com>
2016-07-12 13:50:20 +0200

    Signup and Profile views

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+
+  def gravatar_for(user)
+    gravatar_id = "7738f5d527a4da1ddd5f13799b418679"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
