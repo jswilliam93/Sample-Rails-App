b0500f94513ff2cc77eba39591e996f5f76a43f4
Joe William <jswilliam@gmail.com>
2016-07-17 17:10:33 +0200

    activation token to user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+User.create(name: "Joe",
+            email: "jswilliam93@gmail.com",
+            password: "123456",
+            password_confirmation: "123456",
+            admin: true,
+            activated: true,
+            activated_at: Time.zone.now)
 99.times do |n|
   name = Faker::Name.name
   email = "example#{n+1}@mail.com"
@@ -5,5 +12,7 @@
   User.create(name: name,
               email: email,
               password: password,
-              password_confirmation: password)
+              password_confirmation: password,
+              activated: true,
+              activated_at: Time.zone.now)
 end
